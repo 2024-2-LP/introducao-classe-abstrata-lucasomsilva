83d97316bd90105b90cbb000fe19f61354995268
lucasomsilva <lucas.msilva@sptech.school>
2024-11-07 20:08:27 -0300

    Finalizando exercício

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,55 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+
+   private List<Figura> figuras;
+
+    public List getFiguras() {
+        return figuras;
+    }
+
+    public void setFiguras(List figuras) {
+        this.figuras = new ArrayList<>();
+    }
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+
+        Double somaDasAreas = 0.0;
+
+        for (Figura figura : figuras) {
+           somaDasAreas += figura.calcularArea();
+        }
+        return somaDasAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20() {
+
+        List<Figura> areasMaiorQue20 = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura.calcularArea() > 20.0) {
+                areasMaiorQue20.add(figura);
+            }
+        }
+        return areasMaiorQue20;
+    }
+
+    public List<Figura> buscarQuadrados() {
+
+        List<Figura> listaDeQuadrados = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura instanceof Quadrado) {
+                listaDeQuadrados.add(figura);
+            }
+        }
+        return listaDeQuadrados;
+    }
 }
